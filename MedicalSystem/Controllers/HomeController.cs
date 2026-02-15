ec334ce047b788bbe30c3b311ef24d3dc6040a12
Anfriy <andrewyarovyk@gmail.com>
2026-02-15 15:29:34 +0200

    Initial commit

---
+++ MedicalSystem/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MedicalSystem.Models;
+
+namespace MedicalSystem.Controllers;
+
+public class HomeController : Controller
+{
+   
+}
